a17c1e612c9624a516f598f2e9d0460fafab7a6d
Saiful Islam <saiful01759@gmail.com>
2015-02-21 22:50:17 +0600

    javascript-1

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function youtube () {
+	window.open('http://www.youtube.com','_newtab')
+}
+function facebook () {
+	window.open('http://www.facebook.com','_newtab')
+}
+function twitter () {
+	window.open('http://www.twitter.com','_newtab')
+}
+function openHtmlPage()
+        {
+            window.open('blank.html','_newtab')
+        }
